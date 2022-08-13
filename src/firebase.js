9911bb6a5f559bf48eb3f427d151f9e801789f84
kayyenl <yenyenlee9@gmail.com>
2022-08-13 15:30:01 +0800

    created the skeleton, ready to make the header

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDBWt6swKdsFmFJWc_eLUmoLuKYJrOXLeU",
+    authDomain: "facebook-clone-7ddae.firebaseapp.com",
+    projectId: "facebook-clone-7ddae",
+    storageBucket: "facebook-clone-7ddae.appspot.com",
+    messagingSenderId: "999831750415",
+    appId: "1:999831750415:web:a0bc09653592f25d06207a",
+    measurementId: "G-N7XJR28HHG"
+  };
