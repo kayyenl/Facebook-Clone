e546993526aef19ca76a6ef383b2aa7e3fb69360
kayyenl <yenyenlee9@gmail.com>
2022-08-13 16:09:55 +0800

    started working on the header, at 42:35

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Header from './components/Header';
 function App() {
   return (
     <div className="app">
+      <h1>Let's build a facebook clone!</h1>
       <Header />
     </div>
   );
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,43 @@
 import React from "react";
+import SearchIcon from '@mui/icons-material/Search';
+import HomeIcon from '@mui/icons-material/Home';
+import FlagIcon from '@mui/icons-material/Flag';
+import SubscriptionsOutlinedIcon from '@mui/icons-material/SubscriptionsOutlined';
+import StorefrontOutlinedIcon from '@mui/icons-material/StorefrontOutlined';
+import SupervisedUserCircleIcon from '@mui/icons-material/SupervisedUserCircle';
+
 
 const Header = () => {
   return (
     <div className="header">
       <div className="header__left">
-        <img
+        <img className="header__left--img"
           src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/51/Facebook_f_logo_%282019%29.svg/600px-Facebook_f_logo_%282019%29.svg.png?20200820101156"
-          alt=""
-        />
+          alt="" />
+        <div className="header__input">
+          <SearchIcon />
+          <input type="text" />
+        </div>
+        
+
       </div>
-      <div className="header__middle"></div>
+      <div className="header__middle">
+        <div className="header__option">
+          <HomeIcon fontSize="large"/>
+        </div>
+        <div className="header__option">
+          <FlagIcon fontSize="large"/>
+        </div>
+        <div className="header__option">
+          <SubscriptionsOutlinedIcon fontSize="large"/>
+        </div>
+        <div className="header__option">
+          <StorefrontOutlinedIcon fontSize="large"/>
+        </div>
+        <div className="header__option">
+          <SupervisedUserCircleIcon fontSize="large"/>
+        </div>
+      </div>
       <div className="header__right"></div>
     </div>
   );
