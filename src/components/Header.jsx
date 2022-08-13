b12a563991ea1bfb55c082a0c5e928b2ed4ec606
kayyenl <yenyenlee9@gmail.com>
2022-08-13 15:44:38 +0800

    coming back to check on the image wordwrap

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,18 @@
-import React from 'react';
+import React from "react";
 
 const Header = () => {
-    return (
-        <div className='header'>
-            <div className="header__left"></div>
-            <div className="header__middle"></div>
-            <div className="header__right"></div>
-        </div>
-    );
-}
+  return (
+    <div className="header">
+      <div className="header__left">
+        <img
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/51/Facebook_f_logo_%282019%29.svg/600px-Facebook_f_logo_%282019%29.svg.png?20200820101156"
+          alt=""
+        />
+      </div>
+      <div className="header__middle"></div>
+      <div className="header__right"></div>
+    </div>
+  );
+};
 
 export default Header;
