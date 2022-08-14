7fe73bd68b97727c0f4ea024b771ddd934880830
kayyenl <yenyenlee9@gmail.com>
2022-08-15 01:44:30 +0800

    going to style the sidebar row

--- src/components/SidebarRow.jsx
+++ src/components/SidebarRow.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import Avatar from '@mui/material/Avatar';
 
-const SidebarRow = (props) => {
+const SidebarRow = ({src, Icon, title}) => {
     return (
-        <div>
-            I am a row
+        <div className='sidebar__row'>
+            {src && <Avatar src={src} />}
+            {Icon && <Icon />}
+
+            <h4>{title}</h4>
         </div>
     );
 }
--- src/pages/Sidebar.jsx
+++ src/pages/Sidebar.jsx
@@ -1,16 +1,26 @@
 import React from 'react';
 import SidebarRow from '../components/SidebarRow';
+import EmojiFlagsIcon from '@mui/icons-material/EmojiFlags';
+import LocalHospitalIcon from '@mui/icons-material/LocalHospital';
+import PeopleIcon from '@mui/icons-material/People';
+import ChatIcon from '@mui/icons-material/Chat';
+import StorefrontIcon from '@mui/icons-material/Storefront';
+import VideoLibraryIcon from '@mui/icons-material/VideoLibrary';
+import ExpandMoreOutlinedIcon from '@mui/icons-material/ExpandMoreOutlined';
 
 const Sidebar = () => {
     return (
         <div className='sidebar'>
-            <SidebarRow title="Pages" />
-            <SidebarRow title="Friends" />
-            <SidebarRow title="Pages" />
-            <SidebarRow title="Pages" />
-            <SidebarRow title="Pages" />
-            <SidebarRow title="Pages" />
-            <SidebarRow title="Pages" />
+            <SidebarRow title='Yen Lee' src="" />
+            <SidebarRow title="COVID-19 Information Center" Icon={LocalHospitalIcon} />
+
+            <SidebarRow title="Pages" Icon={EmojiFlagsIcon}/>
+            <SidebarRow title="Friends" Icon={PeopleIcon}/>
+            <SidebarRow title="Messenger" Icon={ChatIcon}/>
+            <SidebarRow title="Marketplace" Icon={StorefrontIcon}/>
+
+            <SidebarRow title="Videos" Icon={VideoLibraryIcon}/>
+            <SidebarRow title="Marketplace" Icon={ExpandMoreOutlinedIcon}/>
         </div>
     );
 }
