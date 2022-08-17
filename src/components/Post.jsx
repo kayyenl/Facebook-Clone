18af7dce8c323230ed5aee1bfdf7507cd3d5d77b
kayyenl <yenyenlee9@gmail.com>
2022-08-18 01:04:52 +0800

    going to take a rest, go get the dummy props tomorrow 2:28:07

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,9 +1,26 @@
+import { Avatar } from '@mui/material';
 import React from 'react';
 
 const Post = ({profilePic, image, username, timestamp, message}) => {
     return (
         <div className='post'>
-            
+            <div className="post__top">
+                <Avatar src={profilePic}
+                className='post__avatar' />
+
+                <div className="post__top--info">
+                    <h3>{username}</h3>
+                    <p>{timestamp}</p>
+                </div>
+
+                <div className="post__bottom">
+                    <p>{message}</p>
+                </div>
+
+                <div className="post__image">
+                    <img src={image} alt="" />
+                </div>
+            </div>
         </div>
     );
 }
