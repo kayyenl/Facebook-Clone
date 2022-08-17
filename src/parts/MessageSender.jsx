62e594098c59fbc2fbff86aaf1fe6acd1e49dc01
kayyenl <yenyenlee9@gmail.com>
2022-08-18 00:47:18 +0800

    did the implementation for forms onchange! learned from google clone

--- src/parts/MessageSender.jsx
+++ src/parts/MessageSender.jsx
@@ -1,21 +1,23 @@
 import { Avatar } from '@mui/material';
-import React from 'react';
+import React, {useState}from 'react';
 import ProfileFace from '../assets/luffy-face.png'
 import VideocamIcon from '@mui/icons-material/Videocam';
 import PhotoLibraryIcon from '@mui/icons-material/PhotoLibrary';
 import InsertEmoticonIcon from '@mui/icons-material/InsertEmoticon';
 
 const MessageSender = () => {
+    const [input, setInput] = useState("");
+
     function handleSubmit(event) {
         event.preventDefault()
-        console.log('hello')
+        console.log(input)
     }
     return (
         <div className='message__sender'>
             <div className="message__sender--top">
                 <Avatar src={ProfileFace} />
                 <form>
-                    <input className='message__sender--input' placeholder={"What's on your mind?"} />
+                    <input className='message__sender--input' placeholder={"What's on your mind?"} value={input} onChange={(event) => setInput(event.target.value)}/>
                     <input placeholder={`Image URL (Optional)`}/>
                     <button onClick={handleSubmit} type='submit'></button>
                 </form>
@@ -24,8 +26,12 @@ const MessageSender = () => {
                 <div className="message__sender--option">
                     <VideocamIcon style={{color: "red"}}/>
                     <h3>Live Video</h3>
+                </div>
+                <div className="message__sender--option">
                     <PhotoLibraryIcon style={{color: "green"}} />
                     <h3>Photo/Video</h3>
+                </div>
+                <div className="message__sender--option">
                     <InsertEmoticonIcon style={{color: "orange"}}/>
                     <h3>Feeling/Activity</h3>
                 </div>
