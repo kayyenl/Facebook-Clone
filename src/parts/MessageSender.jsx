abbc9c573f0cc9a70821d0cdea9c3807da3adb68
kayyenl <yenyenlee9@gmail.com>
2022-08-17 23:30:35 +0800

    2:02:54 still doing the messagesender

--- src/parts/MessageSender.jsx
+++ src/parts/MessageSender.jsx
@@ -1,8 +1,15 @@
 import { Avatar } from '@mui/material';
 import React from 'react';
 import ProfileFace from '../assets/luffy-face.png'
+import VideocamIcon from '@mui/icons-material/Videocam';
+import PhotoLibraryIcon from '@mui/icons-material/PhotoLibrary';
+import InsertEmoticonIcon from '@mui/icons-material/InsertEmoticon';
 
 const MessageSender = () => {
+    function handleSubmit(event) {
+        event.preventDefault()
+        console.log('hello')
+    }
     return (
         <div className='message__sender'>
             <div className="message__sender--top">
@@ -13,7 +20,16 @@ const MessageSender = () => {
                     <button onClick={handleSubmit} type='submit'></button>
                 </form>
             </div>
-            <div className="message__sender--bottom"></div>
+            <div className="message__sender--bottom">
+                <div className="message__sender--option">
+                    <VideocamIcon style={{color: "red"}}/>
+                    <h3>Live Video</h3>
+                    <PhotoLibraryIcon style={{color: "green"}} />
+                    <h3>Photo/Video</h3>
+                    <InsertEmoticonIcon style={{color: "orange"}}/>
+                    <h3>Feeling/Activity</h3>
+                </div>
+            </div>
         </div>
     );
 }
