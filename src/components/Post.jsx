3bb6c10a01085be64bfe7aa8e1288fad9f6be507
kayyenl <yenyenlee9@gmail.com>
2022-08-18 17:49:31 +0800

    trying to make the widget now

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,5 +1,10 @@
 import { Avatar } from '@mui/material';
 import React from 'react';
+import ThumbUpIcon from '@mui/icons-material/ThumbUp';
+import ChatBubbleOutlineIcon from '@mui/icons-material/ChatBubbleOutline';
+import NearMeIcon from '@mui/icons-material/NearMe';
+import ExpandMoreOutlinedIcon from '@mui/icons-material/ExpandMoreOutlined';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 
 const Post = ({profilePic, image, username, timestamp, message}) => {
     return (
@@ -20,6 +25,25 @@ const Post = ({profilePic, image, username, timestamp, message}) => {
 
                 <div className="post__image">
                     <img src={image} alt="" />
+                </div>
+
+                <div className="post__options">
+                    <div className="post__option">
+                        <ThumbUpIcon />
+                        <p>Like</p>
+                    </div>
+                    <div className="post__option">
+                        <ChatBubbleOutlineIcon />
+                        <p>Comment</p>
+                    </div>
+                    <div className="post__option">
+                        <NearMeIcon />
+                        <p>Share</p>
+                    </div>
+                    <div className="post__option">
+                        <AccountCircleIcon />
+                        <ExpandMoreOutlinedIcon />
+                    </div>
                 </div>
         </div>
     );
