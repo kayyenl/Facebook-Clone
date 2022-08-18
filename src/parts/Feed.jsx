701e7e0aad4856714bcc034485b33b4007719760
kayyenl <yenyenlee9@gmail.com>
2022-08-18 17:19:50 +0800

    got my dummy props, going to keep grinding

--- src/parts/Feed.jsx
+++ src/parts/Feed.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Post from '../components/Post';
 import StoryReel from '../components/StoryReel';
 import MessageSender from './MessageSender';
+import ProfileFace from '../assets/luffy-face.png'
 
 const Feed = () => {
     return (
@@ -9,7 +10,12 @@ const Feed = () => {
             <StoryReel />
             <MessageSender />
 
-            <Post />
+            <Post profilePic={ProfileFace}
+            username="Monkey D. Luffy"
+            message="The straw hats have been formed!"
+            timestamp="12:55 PM" 
+            image="https://sportshub.cbsistatic.com/i/2021/08/09/f9f487fc-3c32-40f6-ba90-dc1ce083c962/one-piece-anime-opening-update-new-straw-hats-jimbei-1274612.jpg"
+            />
             <Post />
             <Post />
         </div>
